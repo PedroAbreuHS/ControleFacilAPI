ef71264c3573d4f91df3979b1370e587f3eed628
Pedro Abreu <pedrohsabreu@pedrohsabreu.com>
2023-10-02 10:29:40 -0300

    First Commit

---
+++ src/ControleFacil.Api/Data/Mappings/UsuarioMap.cs
@@ -0,0 +1,30 @@
+﻿using ControleFacil.Api.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ControleFacil.Api.Data.Mappings
+{
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("usuario") //criar tabela
+                .HasKey(x => x.Id); //informar id como chave
+
+            builder.Property(x => x.Email)
+                .HasColumnType("VARCHAR")
+                .IsRequired();
+
+            builder.Property(x => x.Senha)
+                .HasColumnType("VARCHAR")
+                .IsRequired();
+
+            builder.Property(x => x.DataCadastro)
+                .HasColumnType("timestamp")
+                .IsRequired();
+
+            builder.Property(x => x.DataInativacao)
+                .HasColumnType("timestamp");
+        }
+    }
+}
