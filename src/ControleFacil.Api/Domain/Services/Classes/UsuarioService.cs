378b38268a5afe048a95b37b8ff03599f90453c0
Pedro Abreu <pedrohsabreu@pedrohsabreu.com>
2023-10-02 15:56:54 -0300

    Created User Interface Services

---
+++ src/ControleFacil.Api/Domain/Services/Classes/UsuarioService.cs
@@ -0,0 +1,6 @@
+﻿namespace ControleFacil.Api.Domain.Services.Classes
+{
+    public class UsuarioService
+    {
+    }
+}
---
+++ src/ControleFacil.Api/Domain/Services/Interfaces/IUsuarioService.cs
@@ -0,0 +1,9 @@
+﻿using ControleFacil.Api.Contract_DTO.Usuario;
+
+namespace ControleFacil.Api.Domain.Services.Interfaces
+{
+    public interface IUsuarioService : IService<UsuarioRequestContract, UsuarioLoginResponseContract, long>
+    {
+        Task<UsuarioLoginResponseContract> Autenticar(UsuarioLoginRequestContract usuarioLoginRequest);
+    }
+}
